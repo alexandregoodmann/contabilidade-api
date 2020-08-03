c9ee080f2702578fe1a16f446ef15a33244d7c68
unknown <alexandregoodmann@gmail.com>
2020-08-03 16:55:42 -0300

    first commit

---
+++ src/main/java/br/com/goodmann/contabilidadeapi/ContabilidadeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.goodmann.contabilidadeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilidadeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilidadeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/goodmann/contabilidadeapi/ContabilidadeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.goodmann.contabilidadeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilidadeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
