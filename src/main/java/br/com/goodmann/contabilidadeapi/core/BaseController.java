9fa2782421d4bd84be4d7318e6dd71cd07bef54a
unknown <alexandregoodmann@gmail.com>
2020-08-23 12:10:12 -0300

    cross origin

--- src/main/java/br/com/goodmann/contabilidadeapi/core/BaseController.java
+++ src/main/java/br/com/goodmann/contabilidadeapi/core/BaseController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CrossOrigin(origins = "http://localhost:4200")
 public abstract class BaseController<T, ID> {
 
 	@Autowired
