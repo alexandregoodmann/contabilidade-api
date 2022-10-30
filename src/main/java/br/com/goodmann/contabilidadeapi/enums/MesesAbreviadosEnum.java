0ec86ad0be380c3cd8d3324b2a59135f1d7cae3b
alexandregoodmann <alexandregoodmann@gmail.com>
2022-10-30 05:53:32 -0300

    files

---
+++ src/main/java/br/com/goodmann/contabilidadeapi/enums/MesesAbreviadosEnum.java
@@ -0,0 +1,5 @@
+package br.com.goodmann.contabilidadeapi.enums;
+
+public enum MesesAbreviadosEnum {
+	JAN, FEV, MAR, ABR, MAI, JUN, JUL, AGO, SET, OUT, NOV, DEZ;
+}
---
+++ src/main/java/br/com/goodmann/contabilidadeapi/enums/MesesEnum.java
@@ -0,0 +1,5 @@
+package br.com.goodmann.contabilidadeapi.enums;
+
+public enum MesesEnum {
+	JANEIRO, FEVEREIRO, MARÇO, ABRIL, MAIO, JUNHO, JULHO, AGOSTO, SETEMBRO, OUTUBRO, NOVEMBRO, DEZEMBRO;
+}
