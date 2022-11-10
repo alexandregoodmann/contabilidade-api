3716a044b401eeec158503fe5c3e73cdf41cdda7
alexandregoodmann <alexandregoodmann@gmail.com>
2022-11-10 16:46:25 -0300

    Tipo conta

--- src/main/java/br/com/goodmann/contabilidadeapi/dto/ExtratoDTO.java
+++ src/main/java/br/com/goodmann/contabilidadeapi/dto/ExtratoDTO.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.goodmann.contabilidadeapi.enums.TipoConta;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class ExtratoDTO {
 	private String banco;
 	private String descricao;
 	private Boolean marcado;
+	private TipoConta tipo;
 	private BigDecimal saldoPrevisto = BigDecimal.ZERO;
 	private BigDecimal saldoEfetivado = BigDecimal.ZERO;
 	private List<LancamentoDTO> lancamentos = new ArrayList<LancamentoDTO>();
