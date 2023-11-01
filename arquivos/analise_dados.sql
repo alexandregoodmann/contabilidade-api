1a0c71b2b13507ffd10b747476a9fa91a92fbe63
alexandregoodmann <alexandregoodmann@gmail.com>
2023-10-31 21:34:25 -0300

    backup

--- arquivos/analise_dados.sql
+++ arquivos/analise_dados.sql
@@ -1,13 +1,16 @@
-select
-	l.id as idlancamento,	l.data,
-	l.descricao,	l.valor,	l.fixo,	l.concluido,
-	l.numero_bradesco ,	l.tipo ,	l.parcelas,
-	c.id as idconta,	c.descricao as conta,
-	c.tipo as tipoconta
-from
-	lancamento l join planilha p on 	p.id = l.id_planilha
-join conta c on 	 c.id = l.id_conta
-where
-	p.id = 8171 and l.id_conta  = 8711;
-	
-select * from conta;
+
+
+delete from lancamento where id in (8708
+,8709,8746,8751,8753
+,8756,8757,8758,8770
+,8811,8812,8813,8814
+,8815,8816,8817,8818);
+
+select l.id from lancamento l join planilha p on 	p.id = l.id_planilha join conta c on 	 c.id = l.id_conta where c.id = 8710
+
+select * from lancamento_label ll ;
+delete from lancamento_label where id_lancamento in (8708
+,8709,8746,8751,8753
+,8756,8757,8758,8770
+,8811,8812,8813,8814
+,8815,8816,8817,8818);
