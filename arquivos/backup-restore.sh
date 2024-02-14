f065ed0ca1a47be986fc0735681ed0683fcdba5d
alexandregoodmann <alexandregoodmann@gmail.com>
2024-02-14 10:11:32 -0300

    backup

--- arquivos/backup-restore.sh
+++ arquivos/backup-restore.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#BACKUP
-docker exec contabilidade-db /usr/bin/mysqldump -u root --password=example --no-tablespaces contabilidade-dev > backup-$1.sql
-
-#RESTORE
+docker exec contabilidade-db /usr/bin/mysqldump -u root --password=example --no-tablespaces contabilidade-prod > backup-$1.sql
 docker exec -i contabilidade-db mysql -u root --password=example contabilidade-dev < backup-$1.sql
