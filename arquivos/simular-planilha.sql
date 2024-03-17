0c9c7bd6c8f47f48d022afa121c54b7c2516daed
alexandregoodmann <alexandregoodmann@gmail.com>
2024-03-17 11:48:01 -0300

    env

--- arquivos/simular-planilha.sql
+++ arquivos/simular-planilha.sql
@@ -6,3 +6,6 @@ select l.id, 'fevereiro2024', l.data, c.descricao, c.tipo, l.descricao, l.tipo, 
 select * from planilha_anual pa where titulo = 'Fevereiro2024';
 			
 select * from planilha_anual order by valor;
+
+select * from lancamento l where fixo is not null and parcelas is not null;
+select * from lancamento l where parcelas is not null;
