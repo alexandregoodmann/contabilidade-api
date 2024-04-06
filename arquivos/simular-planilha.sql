880b1bfc83ef20bcc9b0a08ddef1a2d70e2ca118
alexandregoodmann <alexandregoodmann@gmail.com>
2024-04-06 10:20:12 -0300

    backup

--- arquivos/simular-planilha.sql
+++ arquivos/simular-planilha.sql
@@ -9,3 +9,10 @@ select * from planilha_anual order by valor;
 
 select * from lancamento l where fixo is not null and parcelas is not null;
 select * from lancamento l where parcelas is not null;
+
+
+select * from `wordcard-prod`.list l ;
+select * from `wordcard-prod`.list_word lw ;
+select wordclass, count(*) as qtd from `wordcard-prod`.word_definition wd group by wordclass order by count(*);
+
+select * from `wordcard-prod`.word_definition wd where wd.wordclass = 'noun';
