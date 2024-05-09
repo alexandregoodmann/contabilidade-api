09bd161461371f1c50f5405f5c2366c85567608a
alexandregoodmann <alexandregoodmann@gmail.com>
2024-05-09 14:51:29 -0300

    backup

--- arquivos/simular-planilha.sql
+++ arquivos/simular-planilha.sql
@@ -5,7 +5,7 @@ select l.id, 'fevereiro2024', l.data, c.descricao, c.tipo, l.descricao, l.tipo, 
 		
 select * from planilha_anual pa where titulo = 'Fevereiro2024';
 			
-select * from planilha_anual order by valor;
+select * from planilha_anual where titulo = 'valor do parto' order by valor;
 
 select * from lancamento l where fixo is not null and parcelas is not null;
 select * from lancamento l where parcelas is not null;
@@ -16,3 +16,8 @@ select * from `wordcard-prod`.list_word lw ;
 select wordclass, count(*) as qtd from `wordcard-prod`.word_definition wd group by wordclass order by count(*);
 
 select * from `wordcard-prod`.word_definition wd where wd.wordclass = 'noun';
+
+
+SELECT * FROM planilha_anual pa WHERE fixo is not null order by valor;
+
+SELECT sum(valor) FROM planilha_anual pa WHERE fixo is not null and titulo = 'Maio2024' order by valor;
