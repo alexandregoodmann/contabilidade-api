8d903a98b9b8fe1103aa6529d34e956b7d1d19c7
alexandregoodmann <alexandregoodmann@gmail.com>
2024-10-05 10:56:42 -0300

    fix label

--- arquivos/backup-restore.sh
+++ arquivos/backup-restore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker exec mysql-db /usr/bin/mysqldump -u root --password=example --no-tablespaces contabilidade-prod > backup-$1.sql
-docker exec -i mysql-db mysql -u root --password=example contabilidade-dev < backup-$1.sql
+docker exec my-mysql /usr/bin/mysqldump -u root --password=mysqlPW --no-tablespaces contabilidade-prod > backup-$1.sql
+docker exec -i my-mysql mysql -u root --password=mysqlPW contabilidade-dev < backup-$1.sql
