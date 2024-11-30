7c7e32ab22f90690728a4d6abdc196ab82f1c1a0
alexandregoodmann <alexandregoodmann@gmail.com>
2024-11-30 10:31:02 -0300

    backup

--- arquivos/backup-restore.bat
+++ arquivos/backup-restore.bat
@@ -1,3 +1,3 @@
 @echo off
-call docker exec mysql-db /usr/bin/mysqldump -u root --password=example --no-tablespaces contabilidade-prod > backup-47.sql
-call docker exec -i mysql-db mysql -u root --password=example contabilidade-dev < backup-47.sql
+call docker exec mysql-db /usr/bin/mysqldump -u root --password=example --no-tablespaces contabilidade-prod > backup-48.sql
+call docker exec -i mysql-db mysql -u root --password=example contabilidade-dev < backup-48.sql
